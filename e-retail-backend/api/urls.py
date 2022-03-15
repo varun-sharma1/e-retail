ebf323744e5aca1a2b8fdd52fa21a61b9fc50401
varun <foo@gmail.com>
2022-03-15 21:39:58 +0530

    Removed unused code

--- e-retail-backend/api/urls.py
+++ e-retail-backend/api/urls.py
@@ -13,6 +13,4 @@ urlpatterns = [
     path('user/', include('api.user.urls')),
     path('order/', include('api.order.urls')),
     path('payment/', include('api.payment.urls')),
-    # path('api-token-auth/', views.obtain_auth_token, name='api_token_auth'), # not using this
-
 ]
