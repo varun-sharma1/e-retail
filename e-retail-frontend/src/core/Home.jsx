38a38970094d6df80edff5f5b53ee4885fe4449b
varun <foo@gmail.com>
2022-03-15 22:12:35 +0530

    rename file

---
+++ e-retail-frontend/src/core/Home.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { Stack } from "react-bootstrap";
+
+import { getProducts } from "./helper/coreapicalls";
+
+import Base from "./Base";
+import CustomizedTables from "./Table";
+import MainNavigation from "./MainNavigation";
+
+import CategorySelect from "./helper/categoryHelper";
+import { totalItemsInCart } from "./helper/cartHelper";
+
+import classes from "./styles/Home.module.css";
+
+// Homepage component
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [currentCount, setCount] = useState(totalItemsInCart());
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchProduct, setSearchedProduct] = useState("");
+  const [addToCartSwitch, setAddToCartSwitch] = useState(false);
+
+  // load all the products sent from the backend.
+  const loadAllProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        setError(data.error);
+        console.log(error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  let productsToDisplay = products;
+
+  if (selectedCategory === "All") {
+    productsToDisplay = products;
+  } else {
+    productsToDisplay = products.filter((product) => {
+      return (
+        product.category.slice(-2)[0].toString() === selectedCategory.toString()
+      );
+    });
+  }
+
+  // condition to filter products
+  if (searchProduct !== "") {
+    productsToDisplay = productsToDisplay.filter((product) => {
+      return product.name.toLowerCase().includes(searchProduct.toLowerCase());
+    });
+  }
+
+  return (
+    <MainNavigation
+      cartCount={currentCount}
+      onSearchProduct={(keyword) => {
+        setSearchedProduct(keyword);
+      }}
+      data-testid="main-navigation"
+    >
+      <Base>
+        <div className={classes.CategoryWrapperDiv}>
+          <Stack gap={1}>
+            <h6>Search By Category</h6>
+            <CategorySelect
+              onChangeCategory={(category) => setSelectedCategory(category)}
+              className={classes.CategorySelectDropdown}
+            />
+          </Stack>
+        </div>
+        <div className="row">
+          <CustomizedTables
+            product={productsToDisplay}
+            changeCount={(count) => setCount(count)}
+            onAddToCart={() => setAddToCartSwitch(!addToCartSwitch)}
+          />
+        </div>
+      </Base>
+    </MainNavigation>
+  );
+};
+
+export default Home;
---
+++ e-retail-frontend/src/core/Payment.jsx
@@ -0,0 +1,175 @@
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
+
+import { cartEmpty, totalCost } from "./helper/cartHelper";
+import { getMeToken, processPayment } from "./helper/paymentHelper";
+import { createOrder } from "./helper/orderHelper";
+import { isAuthenticated, signout } from "../auth/helper";
+
+import DropIn from "braintree-web-drop-in-react";
+
+// payment component for Braintree
+const PaymentB = (products, reload = false, setReload = (f) => f) => {
+  const [info, setInfo] = useState({
+    loading: false,
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {},
+  });
+
+  const [orderSuccess, setOrderSuccess] = useState(false);
+
+  const userId = isAuthenticated() && isAuthenticated().user.id;
+  let token;
+
+  if (isAuthenticated()) {
+    token = JSON.parse(localStorage.getItem("token")).token.toString();
+  }
+
+  products = JSON.parse(localStorage.getItem("cart"));
+
+  // get the token from the frontend.
+  const getToken = (userId, token) => {
+    // fetch the token from the user.
+    getMeToken(userId, token).then((info) => {
+      if (info.error) {
+        setInfo({
+          ...info,
+          error: info.error,
+        });
+        signout(() => {
+          return <Redirect to="/" />;
+        });
+      } else {
+        const clientToken = info.clientToken;
+        setInfo({ clientToken });
+      }
+    });
+  };
+
+  useEffect(() => {
+    getToken(userId, token);
+  }, [userId, token]);
+
+  // function to be executed when the purchase button is clicked.
+  const onPurchase = () => {
+    setInfo({ loading: true });
+    let nonce;
+
+    info.instance.requestPaymentMethod().then((data) => {
+      nonce = data.nonce;
+      const paymentData = {
+        paymentMethodNonce: nonce,
+        amount: totalCost(),
+      };
+
+      // process the payment input by user.
+      processPayment(userId, token, paymentData)
+        .then((response) => {
+          if (response.error) {
+            if (response.code === "200") {
+              signout(() => {
+                return <Redirect to="/" />;
+              });
+            }
+          } else {
+            setInfo({ ...info, success: response.success, loading: true });
+
+            setOrderSuccess(true);
+
+            let productNames = JSON.stringify(products);
+
+            const orderData = {
+              products: productNames,
+              transaction_id: response.transaction.id,
+              amount: response.transaction.amount,
+            };
+
+            // create a new order.
+            createOrder(userId, token, orderData)
+              .then((response) => {
+                if (response.error) {
+                  if (response.code === "200") {
+                    signout(() => {
+                      return <Redirect to="/" />;
+                    });
+                  }
+                } else {
+                  if (response.success === true) {
+                    console.log("ORDER PLACED!!");
+                  }
+                }
+              })
+              .catch((error) => {
+                setInfo({ loading: false, success: false });
+                console.log("Order FAILED", error);
+              });
+
+            cartEmpty(() => {});
+
+            setReload(!reload);
+          }
+        })
+        .catch((error) => {
+          setInfo({ loading: false, success: false });
+        });
+    });
+  };
+
+  // message to display when order was successfully created.
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: orderSuccess ? "" : "none" }}
+          >
+            New order created successfully. Go back to <Link to="/">Home</Link>.
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const purchaseButton = info.loading ? (
+    <button onClick={onPurchase} className="btn btn-block btn-success" disabled>
+      Buy Now
+    </button>
+  ) : (
+    <button onClick={onPurchase} className="btn btn-block btn-success">
+      Buy Now
+    </button>
+  );
+
+  // web dropin for braintree
+  const showbtnDropIn = () => {
+    return (
+      <div>
+        {info.clientToken !== null && products.length > 0 ? (
+          <div>
+            <DropIn
+              options={{ authorization: info.clientToken }}
+              onInstance={(instance) => (info.instance = instance)}
+            />
+            {purchaseButton}
+          </div>
+        ) : (
+          <h3>Please login first or add something in cart</h3>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {orderSuccess && successMessage()}
+      <h3>Your bill is $ {totalCost()}</h3>
+      {showbtnDropIn()}
+    </div>
+  );
+};
+
+export default PaymentB;
