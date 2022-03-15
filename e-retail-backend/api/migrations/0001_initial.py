ccc67d0e3359dcba24e4192e545497fe5c5b5ba7
varun <foo@gmail.com>
2022-03-15 21:24:56 +0530

    Autogenerated files

---
+++ e-retail-backend/api/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+
+from api.user.models import CustomUser
+
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user = CustomUser(
+            name="varun",
+            email="varun@gmail.com",
+            is_staff=True,
+            is_superuser=True,
+            phone="9876543210",
+            gender="Male"
+        )
+
+        user.set_password("Abcd@123")
+        user.save()
+
+    dependencies = [
+
+    ]
+
+    operations = [
+        migrations.RunPython(seed_data),
+    ]
